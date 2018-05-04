f752a3e7cac94fd7ce44558098498532f417ee94
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-04 03:16:45 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,17 @@
-
-
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
-  def initialize(brand)
-    @brand = brand
-  end
-
-  def cobble
-    puts "Your shoe is as good as new!"
-    self.condition = "new"
-  end
 end
+
+
+#class Shoe
+  #attr_accessor :color, :size, :material, :condition
+#  attr_reader :brand
+
+  #def initialize(brand)
+#    @brand = brand
+#  end
+
+#  def cobble
+#    puts "Your shoe is as good as new!"
+  #  self.condition = "new"
+#  end
+#end
